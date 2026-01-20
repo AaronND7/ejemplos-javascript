cb2b61084038538d01622ed68cfc2ac01386df48
AaronND7 <bystaxx814@gmail.com>
2026-01-20 14:46:23 -0600

    Retrieve value from input element in mostrar function

--- calculadora.js
+++ calculadora.js
@@ -1,7 +1,7 @@
 // alert("Hola mundo");
 
 function mostrar() {
-  let numero1 = document.getElementById("numero1");
+  let numero1 = document.getElementById("numero1").value;
 
   alert(numero1);
 }
