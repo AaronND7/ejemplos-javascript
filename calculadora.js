df9ddb2026fb9d518d178e505e5319aec67b90b7
AaronND7 <bystaxx814@gmail.com>
2026-01-20 14:49:34 -0600

    Add alert for second number in mostrar function

--- calculadora.js
+++ calculadora.js
@@ -2,6 +2,8 @@
 
 function mostrar() {
   let numero1 = document.getElementById("numero1").value;
+  let numero2 = document.getElementById("numero2").value
 
   alert(numero1);
+  alert(numero2);
 }
