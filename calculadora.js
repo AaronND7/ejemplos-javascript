84574cbbb7e33799ce558b24240e01bff8d91668
AaronND7 <bystaxx814@gmail.com>
2026-01-20 14:51:09 -0600

    Calculate and display the sum of two numbers

--- calculadora.js
+++ calculadora.js
@@ -4,6 +4,8 @@ function mostrar() {
   let numero1 = document.getElementById("numero1").value;
   let numero2 = document.getElementById("numero2").value
 
-  alert(numero1);
-  alert(numero2);
+  let resultado = 0;
+  resultado = numero1 + numero2;
+
+  alert(resultado);
 }
