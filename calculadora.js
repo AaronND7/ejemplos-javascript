7828663f867853621f9c77db0fc6f40adac7b688
AaronND7 <bystaxx814@gmail.com>
2026-01-20 14:53:05 -0600

    Parse input values as integers in mostrar function

--- calculadora.js
+++ calculadora.js
@@ -1,8 +1,8 @@
 // alert("Hola mundo");
 
 function mostrar() {
-  let numero1 = document.getElementById("numero1").value;
-  let numero2 = document.getElementById("numero2").value
+  let numero1 = parseInt(document.getElementById("numero1").value);
+  let numero2 = parseInt(document.getElementById("numero2").value);
 
   let resultado = 0;
   resultado = numero1 + numero2;
