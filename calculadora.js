d7ef91c211e9bbe8695a2821156c039e9d977ebf
AaronND7 <bystaxx814@gmail.com>
2026-01-20 14:58:48 -0600

    Fix resultado assignment to use DOM element

--- calculadora.js
+++ calculadora.js
@@ -4,7 +4,7 @@ function mostrar() {
   let numero1 = parseInt(document.getElementById("numero1").value);
   let numero2 = parseInt(document.getElementById("numero2").value);
 
-  let resultado = 0;
+  let resultado = document.getElementById("resultado");
   resultado = numero1 + numero2;
 
   alert(resultado);
